85577ec8c274d5c9404f38a8e92bae2a58533912
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2025-12-04 16:31:09 -0400

    inicializando el formulario

---
+++ src/app/personas/listado-personas/listado-personas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-personas',
+  imports: [],
+  templateUrl: './listado-personas.html',
+  styleUrl: './listado-personas.css',
+})
+export class ListadoPersonas {
+
+}
